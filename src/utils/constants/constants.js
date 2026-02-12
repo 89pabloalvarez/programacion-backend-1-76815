f9b8a34813e6a953cfb1fea7590655f9a8bbe8fd
DESKTOP-MAIN\Pablo Alvarez <89.pablo.alvarez@gmail.com>
2026-02-12 17:37:09 -0300

    Agrego las constantes que voy a utilizar en los distintos módulos de productos y carritos de compras.

--- src/utils/constants/constants.js
+++ src/utils/constants/constants.js
@@ -1,4 +1,8 @@
 export const CONSTANTS = {
-  PORT: 8080,
-  BASEURL: `http://localhost:8080`
+    PORT: 8080,
+    BASEURL: `http://localhost:8080`,
+    DIR_URL_PRODUCTS: '/api/products',
+    DIR_URL_CARTS: '/api/carts',
+    PRODUCT_NOT_FOUND: 'Producto no encontrado.',
+    PURCHASE_NOT_FOUND: 'Compra no encontrada.'
 }
