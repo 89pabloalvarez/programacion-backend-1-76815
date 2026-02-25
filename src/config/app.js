5179b0fd466d63786249273af9495d2b8406af67
DESKTOP-MAIN\Pablo Alvarez <89.pablo.alvarez@gmail.com>
2026-02-25 13:58:51 -0300

    Agrego un middleware para logear el request y el response que se invoca a la api express y lo implemento en la config de la app.

--- src/config/app.js
+++ src/config/app.js
@@ -1,7 +1,11 @@
 import express from 'express'
+import { loggerRequest, loggerResponse } from '../middlewares/logger.js'
 
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+app.use(loggerRequest)
+app.use(loggerResponse)
 
 export default app
