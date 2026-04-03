7165b64d849bbc6d5d2c26510e40364d0c732621
DESKTOP-MAIN\Pablo Alvarez <89.pablo.alvarez@gmail.com>
2026-04-03 14:29:58 -0300

    Cambio el boolean "True"/"False" por "Si"/"No" para una mejor vision del usuario.

--- src/views/views-router.js
+++ src/views/views-router.js
@@ -4,9 +4,13 @@ import { productsService } from '../services/products.js'
 const router = Router()
 
 router.get("/", async (req, res) => {
-  const products = await productsService.getAllWithoutPagination({})
+  let products = await productsService.getAllWithoutPagination({})
   // const products = await productsService.getAll({}) En el caso de usar paginación se descomenta ésta linea y la 12 y listooo!!
   // Devolveríííía los 10 primeros, pero como la parte de la view en websocket no manejo paginado, que quede como estaba originalmente. en la entrega 2. (ésto no se evalúa en la entrega final)
+  products = products.map(p => ({
+    ...p,
+    status: p.status ? 'Si' : 'No'
+  }))
   res.render('pages/home', { 
     page_title: 'Inicio',
     products
