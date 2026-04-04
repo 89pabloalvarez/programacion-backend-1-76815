d03c69736bab14ff0f05f9ef302169b4213b4fb5
DESKTOP-MAIN\Pablo Alvarez <89.pablo.alvarez@gmail.com>
2026-04-04 14:04:12 -0300

    Optimizo la lista de products en la vista, sacando el uso redundante de la variable de products

--- src/views/views-router.js
+++ src/views/views-router.js
@@ -7,14 +7,13 @@ router.get("/", async (req, res) => {
   let products = await productsService.getAllWithoutPagination({})
   // const products = await productsService.getAll({}) En el caso de usar paginación se descomenta ésta linea y la 12 y listooo!!
   // Devolveríííía los 10 primeros, pero como la parte de la view en websocket no manejo paginado, que quede como estaba originalmente. en la entrega 2. (ésto no se evalúa en la entrega final)
-  products = products.map(p => ({
-    ...p,
-    status: p.status ? 'Si' : 'No'
-  }))
   res.render('pages/home', { 
     page_title: 'Inicio',
-    products
-    // products: products.docs 
+    products: products.map(p => ({
+      ...p,
+      status: p.status ? 'Si' : 'No'
+    }))
+    // products: products.docs
   })
 })
 
